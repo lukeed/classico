30e262fc3435cc12c0a772f0d204b7c369fd802a
Luke Edwards <luke.edwards05@gmail.com>
2018-10-29 16:10:15 -0700

    add module logic

---
+++ src/index.js
@@ -0,0 +1,30 @@
+export function toRegex(str) {
+	return new RegExp(`(?<=\\s|^)${str}(?=\\s|$)`);
+}
+
+export function has(node, str) {
+	return toRegex(str).test(node.className);
+}
+
+export function add(node) {
+	var i=1, len=arguments.length;
+	for (; i < len; i++) {
+		has(node, arguments[i]) || (node.className += ` ${arguments[i]}`);
+	}
+}
+
+export function remove(node) {
+	var i=1, len=arguments.length;
+	for (; i < len; i++) {
+		replace(node, arguments[i], ' ');
+	}
+}
+
+export function toggle(node, str, bool) {
+	bool = bool == null ? !has(node, str) : bool;
+	(bool ? add : remove)(node, str);
+}
+
+export function replace(node, old, nxt) {
+	node.className = node.className.replace(toRegex(old), nxt);
+}
